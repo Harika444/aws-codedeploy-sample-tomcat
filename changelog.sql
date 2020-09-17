799d222db6ca97830d9899824d80456599c48bab
Harika444 <47811739+Harika444@users.noreply.github.com>
2020-09-17 14:24:36 +0530

    Create changelog.sql

---
+++ changelog.sql
@@ -0,0 +1,34 @@
+--liquibase formatted sql
+--changeset ashwani:01
+create table test3 ( 
+id int primary key,
+name varchar(255)
+);
+--rollback drop table test3;
+--changeset ashwani:2  
+create  table  test1  (  
+    id  int  primary  key,  
+    name  varchar(255)  
+);  
+--rollback drop table test1;  
+
+--changeset nvoxland:3  
+insert  into  test1  (id,  name)  values  (1,  'Harika');  
+insert  into  test1  (id,  name)  values  (2,  'Yashaswini');
+--rollback DELETE FROM test1 WHERE id IN (1,2);
+--changeset nvoxland:4  
+insert  into  test1  (id,  name)  values  (3,  'Los Angeles');  
+insert  into  test1  (id,  name)  values  (4,  'Cincinnati');
+--rollback DELETE FROM test1 WHERE id IN (3,4);
+--changeset harika:5  
+insert  into  test3  (id,  name)  values  (1,  'India');  
+insert  into  test3  (id,  name)  values  (2,  'Hyderabad');
+--rollback DELETE FROM test3 WHERE id IN (1,2);
+--changeset harika:6  
+insert  into  test3  (id,  name)  values  (4,  'India');  
+insert  into  test3  (id,  name)  values  (5,  'Bangalore');
+--rollback DELETE FROM test3 WHERE id IN (4,5);
+--changeset harika:7 
+insert  into  test1  (id,  name)  values  (6,  'DevOps');  
+insert  into  test1  (id,  name)  values  (7,  'Shubham');
+--rollback DELETE FROM test1 WHERE id IN (6,7);
